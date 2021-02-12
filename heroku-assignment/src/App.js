a34a7e603600f8ed25b22a47985b88ae796595d6
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-02-11 19:03:40 -0500

    changed client to heroku-assignment

--- heroku-assignment/src/App.js
+++ heroku-assignment/src/App.js
@@ -1,24 +1,25 @@
-import { useState, useEffect } from 'react'
-import Acts from './img/acts.png'
+import { useState, useEffect } from 'react';
+import Acts from './img/acts.png';
 import './App.css';
 
 function url(path) {
-  return process.env.MODE_ENV === "development" ?
-  `http://localhost:3101${path}` : path
+  return process.env.MODE_ENV === 'development'
+    ? `http://localhost:3101${path}`
+    : path;
 }
 
 function App() {
-  const [data, setData] = useState("")
+  const [data, setData] = useState('');
   useEffect(() => {
-    fetch(url("/api/*"))
-    .then(res => res.json())
-    .then(apiData => setData(apiData.data))
-  }, [])
+    fetch(url('/api/*'))
+      .then((res) => res.json())
+      .then((apiData) => setData(apiData.data));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-      <img src={Acts} alt="starwars"/>
-      Fortune Says: {data}
+    <div className='App'>
+      <header className='App-header'>
+        <img src={Acts} alt='starwars' />
+        Fortune Says: {data}
       </header>
     </div>
   );
